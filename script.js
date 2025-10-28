bb3b4dcb86c5c715034a892c77a9a2072a5ced61
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-28 18:53:58 +0530

    page 2 animation done

--- script.js
+++ script.js
@@ -33,43 +33,103 @@ function firstpage() {
 }
 // firstpage()
 
-let tl = gsap.timeline()
+// ------------------------------- page 2 js -----------------------------
 
 gsap.from("#section-2-top #btn-1", {
     x: -200,
     opacity: 0,
-    duration: 1,
+    ease: "sine.in",
+    duration: 2,
     scrollTrigger: {
-        trigger: "#btn-1",
+        trigger: "#section-2-top #btn-1",
         scroller: "body",
-        markers: true,
-        start: "top 50%",
-        end: "top 50%",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 2,
     }
 })
 
 gsap.from("#section-2-top #btn-2", {
-    y: -100,
+    y: 50,
     opacity: 0,
-    duration: 1,
+    ease: "sine.in",
     scrollTrigger: {
-        trigger: "#btn-1",
+        trigger: "#section-2-top #btn-2",
         scroller: "body",
-        markers: true,
-        start: "top 50%",
-        end: "top 50%",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 2
     }
 })
 
 gsap.from("#section-2-top #btn-3", {
     x: 200,
     opacity: 0,
-    duration: 1,
+    ease: "sine.in",
     scrollTrigger: {
-        trigger: "#btn-1",
+        trigger: "#section-2-top #btn-3",
         scroller: "body",
-        markers: true,
-        start: "top 50%",
-        end: "top 50%",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 2
+    }
+})
+
+gsap.from("#section-2-bottom-left #circle-2", {
+    x: -200,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+        trigger: "#section-2-bottom-left #circle-2",
+        scroller: "body",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%"
+    }
+})
+
+gsap.from("#section-2-bottom-left #circle-1", {
+    x: 200,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "sine.out",
+    scrollTrigger: {
+        trigger: "#section-2-bottom-left #circle-1",
+        scroller: "body",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%"
+    }
+})
+
+gsap.from("#section-2-bottom-left #circle-3,#section-2-bottom-left img", {
+    scale: 1.05,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 2,
+    ease: "sine.out",
+    scrollTrigger: {
+        trigger: "#section-2-bottom-left #circle-3,#section-2-bottom-left img",
+        scroller: "body",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%"
+    }
+})
+
+gsap.from("#section-2-bottom-right #experience,#education,#interests", {
+    x: 200,
+    opacity: 0,
+    stagger: 0.3,
+    ease: "sine.out",
+    scrollTrigger: {
+        trigger: "#section-2-bottom-left #circle-3,#section-2-bottom-left img",
+        scroller: "body",
+        // markers: true,
+        start: "top 60%",
+        end: "top 55%",
     }
 })
